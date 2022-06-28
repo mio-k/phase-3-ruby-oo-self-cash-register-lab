e7b6c2d5b2f2bdf09dc38073edc45d3dfb8aa08e
mio kataoka <mio.kanna@gmail.com>
2022-06-27 17:02:18 -0700

    commit tree 968b4f28b3dec0bee9fcf01d2ea3b9e4fa383018

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
 
+class CashRegister
+    attr_reader :total
+    attr_accessor :discount, :product, :items 
+    def initialize(discount=0)
+        @total=0
+        @discount=discout
+        @items=[]
+    end
+    
+    def add_item(product, price, quantity=1)
+        if quantity=!1
+            @total += (price*quantity)
+        else
+            @total += price
+    end
+
+    def apply_discount
+        # if self.discount != 0
+        #     self.total 
+    end
+end
